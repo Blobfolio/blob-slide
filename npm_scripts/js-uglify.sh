54fd026a43f1349de35b916f78c9235fcb2d7776
Josh Stoik <josh@blobfolio.com>
2018-06-16 11:51:42 -0700

    replace grunt with npm scripts

---
+++ npm_scripts/js-uglify.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+#
+# NPM: Uglify Tasks
+#
+# These are a little too cumbersome to deal with inside NPM.
+##
+
+
+
+# Check dependencies.
+command -v uglifyjs >/dev/null 2>&1 || {
+	echo -e "\033[31;1mError:\033[0m uglifyjs must be in \$PATH."
+	echo -e "\033[96;1mFix:\033[0m npm i uglify-es -g"
+	exit 1
+}
+
+
+
+# Just one file.
+uglifyjs -c --ecma 5 -o "blob-slide.min.js" -- "src/blob-slide.js"
+echo -e "\033[2mminifying:\033[0m blob-slide.min.js"
+
+
+
+# We're done!
+echo -e "\033[32;1mSuccess:\033[0m Uglification has completed!"
+exit 0
