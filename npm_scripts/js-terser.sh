c53fb712bb718e44715ff15887a6f6e04c4046a4
Josh Stoik <josh@blobfolio.com>
2019-08-24 12:21:05 -0700

    switch to terser

--- npm_scripts/js-terser.sh
+++ npm_scripts/js-terser.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #
-# NPM: Uglify Tasks
+# NPM: Terser Tasks
 #
 # These are a little too cumbersome to deal with inside NPM.
 ##
@@ -8,16 +8,16 @@
 
 
 # Check dependencies.
-command -v uglifyjs >/dev/null 2>&1 || {
-	echo -e "\033[31;1mError:\033[0m uglifyjs must be in \$PATH."
-	echo -e "\033[96;1mFix:\033[0m npm i uglify-es -g"
+command -v terser >/dev/null 2>&1 || {
+	echo -e "\033[31;1mError:\033[0m terser must be in \$PATH."
+	echo -e "\033[96;1mFix:\033[0m npm i terser -g"
 	exit 1
 }
 
 
 
 # Just one file.
-uglifyjs -c --ecma 6 -o "blob-slide.min.js" -- "src/blob-slide.js"
+terser -c -m -o "blob-slide.min.js" -- "src/blob-slide.js"
 echo -e "\033[2mminifying:\033[0m blob-slide.min.js"
 
 
